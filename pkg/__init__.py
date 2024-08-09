4f1cb62a52241930ec4ad52cc15d9825b98df71f
Ben Thornton <ben.thornton@hexagon.com>
2024-08-09 16:46:32 -0400

    added pip scripts

---
+++ pkg/__init__.py
@@ -0,0 +1,10 @@
+# ---------------------
+version = '0.0.1'
+commit_message = '''added pip scripts'''
+date = 'August 9th, 2024'
+# ---------------------
+author = 'Ben Thornton'
+author_email = 'bthorn191@gmail.com'
+name = 'rescalepy'
+description = 'a Python client library for the Rescale API'
+install_requires = ['requests', 'keyring']
