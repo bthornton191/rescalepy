81822c08b8671d5248c551818711ece701e9cd62
Ben Thornton <ben.thornton@hexagon.com>
2026-01-23 09:59:29 -0500

    Made the endpoint an instance variable so that it can be set per client. Created an `itar` boolean parameter for the client to automatically use the itar endpoint.

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,5 +1,5 @@
 # ---------------------
-version = '0.1.0'
+version = '0.1.1'
 commit_message = '''Made the endpoint an instance variable so that it can be set per client. Created an `itar` boolean parameter for the client to automatically use the itar endpoint.'''
 date = 'January 23rd, 2026'
 # ---------------------
